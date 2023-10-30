1a91fc6eb50b4cf99e07bade7e8f6abb4149d276
vern-gora <danvern99@gmail.com>
2023-10-30 21:47:05 +0200

    done

---
+++ models/Contact.js
@@ -0,0 +1,45 @@
+import { Schema, model } from "mongoose";
+
+import { handleSaveError, runValidatorsAtUpdate } from "./hooks.js";
+
+import Joi from "joi";
+
+
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+      favorite: {
+        type: Boolean,
+        default: false,
+      },
+}, {versionKey: false})
+
+contactSchema.post("save", handleSaveError);
+
+contactSchema.pre("findOneAndUpdate", runValidatorsAtUpdate);
+
+contactSchema.post("findOneAndUpdate", handleSaveError);
+
+export const contactsAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.number().required(),
+  favorite: Joi.boolean()
+})
+
+export const movieUpdateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required()
+})
+
+
+const Contact = model("contact", contactSchema);
+
+export default Contact;
